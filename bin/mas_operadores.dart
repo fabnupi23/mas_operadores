85b5fdd069d7207c9b84e1f985ed4c3a261cc3a9
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2024-01-26 11:26:12 -0500

    Operador Null Aware

---
+++ bin/mas_operadores.dart
@@ -0,0 +1,7 @@
+import 'package:mas_operadores/mas_operadores.dart' as mas_operadores;
+
+void main() {
+  String? name;
+
+  //Operador Null Aware
+}
---
+++ test/mas_operadores_test.dart
@@ -0,0 +1,8 @@
+import 'package:mas_operadores/mas_operadores.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
